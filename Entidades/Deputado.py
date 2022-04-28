5949d8cd2d0c256906bcf4e85e1af6f98e946e61
Anon1929 <anon89345@outlook.com>
2022-04-28 13:59:12 -0300

    remove_entities

--- Entidades/Deputado.py
+++
@@ -1,6 +0,0 @@
-class Deputado:
-    def __init__(self, politico, leiaprovada):
-        self.Id = politico.Id
-        self.Salario = politico.Salario
-        self.Nome = politico.Nome
-        self.LeiAprovada = leiaprovada
--- Main.py
+++ Main.py
@@ -1,13 +1,4 @@
 from Persistencia.Persistencia import Persistencia
-<<<<<<< HEAD
 from Apresentacao.Apresentacao import Apresentacao
 test = Persistencia()
 test2 = Apresentacao()
-=======
-#from Negocio.Negocio import Negocio
-from Apresentacao.Apresentacao import Apresentacao
-
-persistence = Persistencia()
-#business = Negocio(persistence)
-presentation = Apresentacao() #Apresentacao(business)
->>>>>>> e0007a72389bf7719d80b54d484e9c1b709fea55
