48cf32826163a3071deb3d34a234a68dec85ff71
Davivi <48662592+d-magit@users.noreply.github.com>
2022-04-29 00:46:03 -0300

    Avaliacao CRUD

--- Persistencia/Repositories/Avaliacao.py
+++ Persistencia/Repositories/Avaliacao.py
@@ -2,3 +2,17 @@ class Avaliacao:
     ## Initialize repository
     def __init__(self, SQLManager):
         self.__sqlManager = SQLManager
+    
+    def CreateEvaluation(self, evaluation):
+        values = f"'{evaluation['Usuario_Nome']}', {evaluation['Politico_ID']}, {evaluation['Nota']}, '{evaluation['Comentario']}'"
+        self.__sqlManager.ExecuteQuery(f"INSERT INTO Avaliacao(Usuario_Nome, Politico_ID, Nota, Comentario) VALUES ({values})")
+    
+    def ListUserEvaluations(self, username):
+        return self.__sqlManager.SelectQuery(f"SELECT * FROM Avaliacao WHERE Usuario_Nome = '{username}'")
+    
+    def UpdateEvaluation(self, evaluation):
+        values = f"Usuario_Nome = '{evaluation['Usuario_Nome']}', Politico_ID = {evaluation['Politico_ID']}, Nota = {evaluation['Nota']}, Comentario = '{evaluation['Comentario']}'"
+        return self.__sqlManager.ExecuteQuery(f"UPDATE Avaliacao SET {values} WHERE Numero_Aval = {evaluation['Numero_Aval']}")
+    
+    def DeleteEvaluation(self, evalId):
+        return self.__sqlManager.ExecuteQuery(f"DELETE FROM Avaliacao WHERE Numero_Aval = {evalId}")
