4489cd5b5d3031026c676d7032c11fdbbbc0b492
Davivi <48662592+d-magit@users.noreply.github.com>
2022-04-30 02:41:08 -0300

    Adding country code to ListPoliticos

--- Persistencia/Repositories/Politico.py
+++ Persistencia/Repositories/Politico.py
@@ -15,10 +15,10 @@ class Politico:
     def GetMediaSalarial(self, countryCode):
         return self.__sqlManager.ExecuteProcedure("Get_Media_Salarial", [countryCode])[-1][0]['@Media_Salarial']
 
-    def ListPoliticos(self):
+    def ListPoliticos(self, countryCode):
         govs = [self.__process_politico(i) for i in self.__sqlManager.ExecuteProcedure("Get_Join_Gov_UF_Pais")[0]]
         deps = [self.__process_politico(i) for i in self.__sqlManager.ExecuteProcedure("Get_Join_Dep_UF_Pais")[0]]
         mins = [self.__process_politico(i) for i in self.__sqlManager.ExecuteProcedure("Get_Join_Min_Mis_Pais")[0]]
         pres = [self.__process_politico(i) for i in self.__sqlManager.ExecuteProcedure("Get_Join_Pres_Pais")[0]]
-        result = sorted(govs + deps + mins + pres, key=lambda x: x['ID'])
+        result = sorted([i for i in govs + deps + mins + pres if i['Pais_Codigo'] == countryCode], key=lambda x: x['ID'])
         return result
